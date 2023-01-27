03a33e6e87a39f5a6be0302a11115aa934f0a31b
AjithkumarSankar <ajithkumar_sankar@premierinc.com>
2023-01-27 20:36:35 +0530

    Updated inventory and order service

--- inventory-service/src/main/java/com/learn/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/learn/inventoryservice/controller/InventoryController.java
@@ -1,13 +1,18 @@
 package com.learn.inventoryservice.controller;
 
+import com.learn.inventoryservice.dto.InventoryResponse;
+import com.learn.inventoryservice.model.Inventory;
 import com.learn.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -19,9 +24,9 @@ public class InventoryController {
     // http://localhost:8082/api/inventory/iphone-13,iphone13-red
 
     // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestBody List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/learn/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/learn/inventoryservice/service/InventoryService.java
@@ -1,10 +1,14 @@
 package com.learn.inventoryservice.service;
 
+import com.learn.inventoryservice.dto.InventoryResponse;
+import com.learn.inventoryservice.model.Inventory;
 import com.learn.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +18,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
+    public List<InventoryResponse> isInStock(List<String> skuCodeList) {
         log.info("Checking Inventory");
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+        return inventoryRepository.findBySkuCodeIn(skuCodeList).stream()
+                .map(inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity() > 0)
+                        .build()).toList();
     }
 }
